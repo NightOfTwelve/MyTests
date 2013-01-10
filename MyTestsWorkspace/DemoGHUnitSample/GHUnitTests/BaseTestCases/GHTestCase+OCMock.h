e286796469655decc0817b417a5eb78e8d30c798
zerocpp <lnddszp@gmail.com>
2013-01-10 23:39:10 +0800

    GHTestCase(OCMock)

---
+++ MyTestsWorkspace/DemoGHUnitSample/GHUnitTests/BaseTestCases/GHTestCase+OCMock.h
@@ -0,0 +1,13 @@
+//
+//  GHTestCase+OCMock.h
+//  DemoGHUnitSample
+//
+//  Created by Song Zhipeng on 1/10/13.
+//  Copyright (c) 2013 Song Zhipeng. All rights reserved.
+//
+
+#import <GHUnitIOS/GHUnit.h>
+
+@interface GHTestCase (OCMock)
+
+@end
---
+++ MyTestsWorkspace/DemoGHUnitSample/GHUnitTests/BaseTestCases/GHTestCase+OCMock.m
@@ -0,0 +1,13 @@
+//
+//  GHTestCase+OCMock.m
+//  DemoGHUnitSample
+//
+//  Created by Song Zhipeng on 1/10/13.
+//  Copyright (c) 2013 Song Zhipeng. All rights reserved.
+//
+
+#import "GHTestCase+OCMock.h"
+
+@implementation GHTestCase (OCMock)
+
+@end
