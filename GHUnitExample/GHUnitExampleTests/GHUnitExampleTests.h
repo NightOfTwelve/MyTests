58a53f75a2ddc48c1a1945f2beeb280c81147183
zerocpp <lnddszp@gmail.com>
2013-01-10 22:15:34 +0800

    home backup

---
+++ GHUnitExample/GHUnitExampleTests/GHUnitExampleTests.h
@@ -0,0 +1,13 @@
+//
+//  GHUnitExampleTests.h
+//  GHUnitExampleTests
+//
+//  Created by Song Zhipeng on 1/10/13.
+//  Copyright (c) 2013 Song Zhipeng. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GHUnitExampleTests : SenTestCase
+
+@end
---
+++ GHUnitExample/GHUnitExampleTests/GHUnitExampleTests.m
@@ -0,0 +1,32 @@
+//
+//  GHUnitExampleTests.m
+//  GHUnitExampleTests
+//
+//  Created by Song Zhipeng on 1/10/13.
+//  Copyright (c) 2013 Song Zhipeng. All rights reserved.
+//
+
+#import "GHUnitExampleTests.h"
+
+@implementation GHUnitExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GHUnitExampleTests");
+}
+
+@end
